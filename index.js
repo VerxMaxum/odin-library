432c2be10b2a20aeecc82dde73d859d6c8b65c7f
VerxMaxum <138268061+VerxMaxum@users.noreply.github.com>
2024-05-30 22:19:23 +0800

    Add initializer

---
+++ index.js
@@ -0,0 +1,60 @@
+// const library = [{title: "IT", author: "Stephen King", pages: "1400"},
+//                  {title: "The Green Mile", author: "Stephen King", pages: "1000"}];
+
+function Book(title, name, pages, read) {
+    this.title = title;
+    this.name = name;
+    this.pages = pages;
+    this.read = read;
+}
+
+const library = [new Book("It", "Stephen King", "1400", "Yes"), new Book("The Green Mile", "Stephen King", "1000", "Yes")];
+
+const main = document.getElementById('main');
+
+for(let book of library) {
+    if(book instanceof Book)
+        console.log("This is a book");
+    else
+        console.log("Not a book");
+    let card = document.createElement('div');
+    card.classList.add('book-container');
+
+    let title = document.createElement('p');
+    title.textContent = "Title: " + book.title;
+
+    let author = document.createElement('p');
+    author.textContent = "Author: " + book.author;
+
+    let pages = document.createElement('p');
+    pages.textContent = "Number of Pages: " + book.pages;
+
+
+    let buttonContainer = document.createElement('div');
+    buttonContainer.classList.add('button-container');
+
+    let readButton = document.createElement('button');
+    readButton.classList.add('book-button', 'read-button');
+    if(book.read === "Yes")
+        readButton.textContent = "Read";
+    else if(book.read === "No")
+        readButton.textContent = "Unread";
+
+    let removeButton = document.createElement('button');
+    removeButton.classList.add('book-button', 'remove-button');
+    removeButton.textContent = "Remove";
+
+    buttonContainer.appendChild(readButton);
+    buttonContainer.appendChild(removeButton);
+
+    card.appendChild(title);
+    card.appendChild(author);
+    card.appendChild(pages);
+    card.appendChild(buttonContainer);
+
+    main.appendChild(card);
+}
+
+function addtoLibrary() {
+
+}
